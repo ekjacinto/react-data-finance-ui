d3f9ad60690dad31d35bc5193c95335c1c37034c
Ethan Jacinto <ethankylejacinto@gmail.com>
2024-01-04 19:58:44 -0800

    feat: more styling for Card component

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,10 +6,10 @@ import Triple from "../assets/triple.png"
 const Cards = () => {
   return (
     <section className="w-full py-[10rem] px-4 bg-white">
-      <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8">
-        <div className="w-full">
+      <div className="max-w-[1240px] mx-auto  grid md:grid-cols-3 gap-8">
+        <div className="w-full shadow-xl flex flex-col p-4 my-4 round-lg hover:scale(105) dureation-300">
           <img className="w-20 mx-auto mt-[-3rem] bg-white" src={Single} alt="/" />
-          <h2>Single User</h2>
+          <h2 className="text-2xl fond-bold text-center py-8">Single User</h2>
           <p>$149</p>
           <div>
             <p>500 GB Storage</p>

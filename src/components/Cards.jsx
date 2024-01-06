73ad37b7e644b0a41239ceb0a93256575c5a62c4
Ethan Jacinto <ethankylejacinto@gmail.com>
2024-01-05 23:58:57 -0800

    fix: styling typos && card content

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -9,7 +9,7 @@ const Cards = () => {
       <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8">
         <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
           <img className="w-20 mx-auto mt-[-3rem] bg-white" src={Single} alt="/" />
-          <h2 className="text-2xl fond-bold text-center py-8">Single User</h2>
+          <h2 className="text-2xl font-bold text-center py-8">Single User</h2>
           <p className="text-center text-4xl font-bold">$149</p>
           <div className="text-center font-medium">
             <p className="py-2 border-b mx-8 mt-8">500 GB Storage</p>
@@ -20,7 +20,7 @@ const Cards = () => {
         </div>
         <div className="w-full shadow-xl bg-gray-100 flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300">
           <img className="w-20 mx-auto mt-[-3rem] bg-transparent" src={Double} alt="/" />
-          <h2 className="text-2xl fond-bold text-center py-8">Single User</h2>
+          <h2 className="text-2xl font-bold text-center py-8">Partnership</h2>
           <p className="text-center text-4xl font-bold">$149</p>
           <div className="text-center font-medium">
             <p className="py-2 border-b mx-8 mt-8">500 GB Storage</p>
@@ -31,7 +31,7 @@ const Cards = () => {
         </div>
         <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
           <img className="w-20 mx-auto mt-[-3rem] bg-white" src={Triple} alt="/" />
-          <h2 className="text-2xl fond-bold text-center py-8">Single User</h2>
+          <h2 className="text-2xl font-bold text-center py-8">Group Account</h2>
           <p className="text-center text-4xl font-bold">$149</p>
           <div className="text-center font-medium">
             <p className="py-2 border-b mx-8 mt-8">500 GB Storage</p>
